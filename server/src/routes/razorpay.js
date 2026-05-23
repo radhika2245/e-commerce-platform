e50cabf322067bca63d0874735d614fb7e8f57df
ankittxydv <ankitxydv26@gmail.com>
2026-05-24 00:02:16 +0530

    fix: removed razorpay endpoints

--- server/src/routes/razorpay.js
+++ server/src/routes/razorpay.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 const ctrl = require('../controllers/razorpayController');
 
-router.post('/create-order', ctrl.createOrder);
-router.post('/verify', ctrl.verifyPayment);
+// router.post('/create-order', ctrl.createOrder);
+// router.post('/verify', ctrl.verifyPayment);
 
 module.exports = router;
